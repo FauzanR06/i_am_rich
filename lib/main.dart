b173e80de5c9d248184e728c9e33c49518d87d8a
FauzanR06 <fauzanm157@gmail.com>
2020-04-11 14:18:34 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+//
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text("I Am Rich"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(64.0),
+            child: Image(
+              image: AssetImage('images/crystal.png'),
+            ),
+          ),
+        ),
+        bottomNavigationBar: Text("by Fauzan"),
+      ),
+    );
+  }
+}
